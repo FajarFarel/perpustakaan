25623cd27f30b6dd88ed71be866ea5d097c67f15
noanissa_09 <150217023+FajarFarel@users.noreply.github.com>
2025-07-06 09:58:55 +0700

    config.py

--- backend_perpustakaan/config.py
+++ backend_perpustakaan/config.py
@@ -1,7 +1,7 @@
 DB_HOST = "localhost"
 DB_USER = "root"
 DB_PASSWORD = "Nova_0920"
-DB_NAME = "perpustakaan_fk"
+DB_NAME = "perpustakaan"
 # Base_URL = "http://10.0.2.228:5000/"
 # Base_URL = "http://192.168.1.105:5000/" 
 # Base_URL = "https://sagiscorp.my.id/backend" # Ganti dengan URL server Anda
